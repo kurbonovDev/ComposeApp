4efd51cbb02f95ed9c1889d9fdf090ce45ca8547
kurbonovDev <muhammadkurbonov25@mail.ru>
2024-09-17 12:21:46 +0500

    Example of simple RcView

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,19 +3,42 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeapp.home.HomeScreen3
 import com.example.composeapp.neco.ListItemsPreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val counter = mutableIntStateOf(0)
-        val checked = mutableStateOf(true)
-        val text = mutableStateOf("Some text")
         setContent {
-            ListItemsPreview()
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                itemsIndexed(
+                    listOf("a","b","c","d")
+                ) {index,item->
+                    Text(
+                        text = "${item}",
+                        fontSize = 32.sp,
+                        modifier = Modifier.padding(vertical = 10.dp)
+                    )
+                }
+
+            }
         }
     }
 }
